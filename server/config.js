32a03da9559348396e4a1c486314ebfd08538708
joel <joelnqwork@gmail.com>
2024-09-23 18:43:29 +0200

    database has been configured

--- server/config.js
+++ server/config.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const dbconnect = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/urlShortener').then(() => {
+    mongoose.connect(process.env.MONGO_URI).then(() => {
       
         console.log('database connected succesfully');
         
